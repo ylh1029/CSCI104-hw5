2af0e8c4f94e82caee614fa673635e93db5e8a33
Lily Hirasawa <lhirasaw@usc.edu>
2025-11-28 15:46:18 -0800

    First comiple: created wordle function and build_all_string function for recursive helper function.

--- wordle.cpp
+++ wordle.cpp
@@ -13,6 +13,42 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
+//One recursive call is responsible for nth location in the string
+void build_all_string(
+    const std::string& in, 
+    std::string floating, 
+    const std::set<std::string>& dict, 
+    std::set<std::string>& result,
+    std::string temp,
+    size_t n){
+        if(n == in.length() && floating.empty()){
+            //If we have filled all the spots and used up all the words from floating, it's valid
+            result.insert(temp);
+        }
+        else{
+            if(in[n] != '-'){
+            //If the particular spot is already determined by in
+                build_all_string(in, floating, dict, result, temp+in[n], n+1);
+            }
+            else{
+                for(int i = 0; i < 26; i++){
+                //Iterate through all alphabet
+                    char curr = 'A' + i;
+                    if(floating.find(curr) != std::string::npos){
+                    //If the particular character is in floating: remove and then recurse, then undo that remove
+                        int index = floating.find(curr);
+                        floating.erase(index);
+                        build_all_string(in, floating, dict, result, temp+(curr), n+1);
+                        floating.push_back(index);
+                    }
+                    else{
+                    //Simply recurse to the next step
+                        build_all_string(in, floating, dict, result, temp+(curr), n+1);
+                    }
+                }
+            }
+        }
+}
 
 
 // Definition of primary wordle function
@@ -21,8 +57,21 @@ std::set<std::string> wordle(
     const std::string& floating,
     const std::set<std::string>& dict)
 {
-    // Add your code here
+    std::set<std::string> result{};
 
+    //Recursion
+    build_all_string(in, floating, dict, result, "", 0);
+
+    std::set<std::string>::iterator it = result.begin();
+    for(; it != result.end(); ++it){
+        //Iterate through the result
+        if(dict.find(*it) == dict.end()){
+        //An element from result doesn't exist in the dictionary: not valid word
+            result.erase(*it);
+        }
+    }
+
+    return result;
 }
 
 // Define any helper functions here
