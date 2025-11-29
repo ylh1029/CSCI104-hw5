e7093d3696f7ecc464fa685398af1dad7300676d
Lily Hirasawa <lhirasaw@usc.edu>
2025-11-28 19:07:45 -0800

    Initial draft done- simple testing passed. Now it's time to use the test files

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,10 +21,16 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool fill_spot(
+    AvailabilityMatrix& avail, 
+    const size_t dailyNeed, 
+    const size_t maxShifts,
+    DailySchedule& sched,
+    size_t row,
+    size_t col,
+    vector<size_t>& workerDayCount);
 
 // Add your implementation of schedule() and other helper functions here
-
 bool schedule(
     const AvailabilityMatrix& avail,
     const size_t dailyNeed,
@@ -38,8 +44,71 @@ bool schedule(
     sched.clear();
     // Add your code below
 
+    AvailabilityMatrix availability = avail;
 
+    //Helps us keep track of how many days each workers has been assigned to 
+    vector<size_t> workerDayCount(avail[0].size(), 0);
 
+    //Setting up sched matrix, filling it up with INVALID_ID
+    for(size_t i = 0; i < avail.size(); i++){
+        vector<Worker_T> row(dailyNeed, INVALID_ID);
+        sched.push_back(row);
+    }
+    
+    bool result = fill_spot(availability, dailyNeed, maxShifts, sched, 0, 0, workerDayCount);
 
+    if(!result){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+
+bool fill_spot(
+    AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched, 
+    size_t day_n,
+    size_t worker,
+    vector<size_t>& workerDayCount){
+
+    if(day_n == avail.size()){
+    //Hit the end of the recursion: all matrix should be filled up
+        return true;
+    }
+
+    else{
+        for(size_t i = 0; i < avail[day_n].size(); i++){
+        //Loop through the list of available workers
+            if(avail[day_n][i]){
+                avail[day_n][i] = 0;
+                sched[day_n][worker] = i;
+                workerDayCount[i]++;
+
+                //Calculate values for the next recursive call
+                size_t nextDay = day_n;
+                size_t nextWorker = worker + 1;
+                if(nextWorker == dailyNeed){
+                    nextDay += 1;
+                    nextWorker = 0;
+                }
+
+                //Recursive call is the current matrix is valid
+                if(workerDayCount[i] <= maxShifts){
+                    if(fill_spot(avail, dailyNeed, maxShifts, sched, nextDay, nextWorker, workerDayCount)){
+                        return true;
+                    }
+                }
+
+                //Backtrack to the original state: next option
+                avail[day_n][i] = 1;
+                sched[day_n][worker] = INVALID_ID;
+                workerDayCount[i]--;
+            }
+        }
+        return false;
+    }
 }
 
